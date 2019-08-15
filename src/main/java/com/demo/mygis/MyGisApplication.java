b9e43d4411dd2cbbf5cb4725fb43c5f685bde5a6
Jose Leiva <jdleiva@backcountry.com>
2019-08-15 17:18:57 -0600

    Setup basico

---
+++ src/main/java/com/demo/mygis/MyGisApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mygis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGisApplication.class, args);
+	}
+
+}
